de6444c1ab19cb7539ced4910a1b86228f4e985b
tmking627 <tmy6254789@gmail.com>
2023-11-25 20:41:00 +0900

    add edit validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -22,6 +22,7 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export type State = {
   errors?: {
+    id?: string[];
     customerId?: string[];
     amount?: string[];
     status?: string[];
@@ -67,14 +68,24 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 
 // Use Zod to update the expected types
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
+    id: formData.get('id'),
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if(!validatedFields.success){
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { id, customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
