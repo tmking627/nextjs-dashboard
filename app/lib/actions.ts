46b5fe6a0aeebb2163cb252b2bac948eba814a95
tmking627 <tmy6254789@gmail.com>
2023-11-26 00:05:33 +0900

    add signin

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,24 @@
 'use server';
 
+import { signIn } from '@/auth';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
   id: z.string(),
